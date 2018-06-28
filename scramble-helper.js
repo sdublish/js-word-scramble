3b50625a4bf24d59a651c7d6193b42206aac7d52
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-28 13:01:58 -0700

    Initial commit

---
+++ scramble-helper.js
@@ -0,0 +1,84 @@
+// Create a function that reverses a word
+
+function reverseWord(word){
+    let backwardsList = [];
+    let charList = word.split("");
+
+    for(let i=charList.length-1; i >=0; i -=1 ){
+        backwardsList.push(charList[i]);
+    }
+
+    return backwardsList.join("");
+}
+
+// Create a function that takes an array of words and returns a new array of the
+ // words with each word reversed.
+
+ function reverseWordArray(wordArray){
+    let reversedArray = [];
+
+    for(let word of wordArray){
+        reversedArray.push(reverseWord(word));
+    }
+
+    return reversedArray;
+ }
+
+// Create a function that returns a random word from an array
+
+function randomWord(wordArray){
+    let randomIndex = Math.floor(Math.random()* wordArray.length);
+    return wordArray[randomIndex];
+}
+
+// Create an array of words and save it to a variable. Using your functions create
+// a second array of reversed words.
+
+// We could use the two arrays we've created to select a random word to show the user
+// and check their guess. How could we do that? Is there a better way?
+
+
+// ////////////////////////////////////////////////////////////////////////////
+
+// Create a function that takes an array of words and returns a map with the keys
+// as the reversed word and the values as the original word.
+
+function createWordMap(wordArray){
+    let wordMap = new Map();
+
+    for (let word of wordArray){
+        wordMap.set(reverseWord(word), word);
+    }
+
+    return wordMap;
+}
+
+
+
+// Create a function that takes two strings, `guess` and `word` and a map, like
+// the one you created above. If the first string is in the map, return the word
+// if not, console log "Sorry, incorrect. The word was ..." and include word.
+
+function validateGuess(guess, word, wordMap){
+    if (wordMap.has(guess)){
+        return word;
+    }
+
+    else{
+        console.log(`Sorry, incorrect. The word was ${word}`)
+    }
+}
+
+
+
+
+// ////////////////////////////////////////////////////////////////////////////
+// FURTHER STUDY
+
+// Create a function that scrambles a word. Use whatever method you like to
+// rearrange the letters.
+
+
+
+// Create a function that takes an array of words and returns a map with the
+// scrambled words as the keys and the original word as the values.
